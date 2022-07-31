92f70f35224d2f7949dd423a2d8b20836b7462ec
Teuthida <33849310+Teuthida255@users.noreply.github.com>
2022-07-31 14:14:21 -0700

    Add variable and functions for final mon music

--- include/battle.h
+++ include/battle.h
@@ -608,6 +608,7 @@ struct BattleStruct
     u8 stickyWebUser;
     u8 appearedInBattle; // Bitfield to track which Pokemon appeared in battle. Used for Burmy's form change
     u8 skyDropTargets[MAX_BATTLERS_COUNT]; // For Sky Drop, to account for if multiple Pokemon use Sky Drop in a double battle.
+    u8 finalMonMusicPlaying;
 };
 
 #define F_DYNAMIC_TYPE_1 (1 << 6)
@@ -779,6 +780,10 @@ struct BattleSpriteData
     struct BattleAnimationInfo *animationData;
     struct BattleBarInfo *battleBars;
 };
+
+u8 GetRemainingOpponentMons();
+bool8 IsOpponentFinalMon();
+u16 GetFinalMonMusic();
 
 #include "sprite.h"
 
